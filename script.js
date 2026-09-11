ec508c5e51172812e30fd221b9772817dadffbd4
Andrezza Cardoso <andrezza.m.cardoso@gmail.com>
2026-09-10 23:23:46 -0300

    style: finaliza responsividade do portfólio

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const titulo = document.querySelector("h1");
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+const menuIcon = menuToggle.querySelector("i");
 
-console.log(titulo);
+menuToggle.addEventListener("click", () => {
+
+    mainNav.classList.toggle("active");
+
+    if(mainNav.classList.contains("active")){
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+
+        menuToggle.classList.add("active");
+    } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+
+        menuToggle.classList.remove("active");
+    }
+
+
+});
